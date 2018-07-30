4fcaa8d129a6f160da43b62ab3b5e83ff5b4111f
Puerco-Potter <Albiach92@gmail.com>
2018-07-30 11:09:04 -0300

    lindo

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/index", name="homepage")
+     */
+    public function index()
+    {
+        return $this->render('index.html.twig', []);
+    }
+}
